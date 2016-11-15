3fcd51e7195ff22f3a5ba814cb2b612d17254a9f
ph2150074 <ph2150074@transcosmos-ph.local>
2016-11-15 14:15:10 +0800

    update style and html

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,6 @@
 	{{{ Html::style("css/global.css") }}}
 	{{{ Html::style("css/animate.css") }}}
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,700">
-	<!-- <link rel="stylesheet" href="https://fonts.google.com/specimen/Montserrat?selection.family=Montserrat:400,700|Open+Sans:400,700"> -->
 	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
@@ -15,6 +14,5 @@
 <body>
 	@yield("content")
 
-	{{{ Html::script("js/") }}}
 </body>
 </html>
